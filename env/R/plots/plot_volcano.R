2277953f26f117e4c4d5d8a9232b7f93ebc87efa
rschwarz <robert.schwarz@leibniz-fli.de>
2025-11-06 14:37:10 +0100

    Create Panel of Volcanos and Ages of DETEs

--- env/R/plots/plot_volcano.R
+++ env/R/plots/plot_volcano.R
@@ -123,3 +123,99 @@ volcanoPlot <- function(df,
     
     return(pl)
 }
+
+
+#' ------------------------------------------------------------------------------
+#' Filter a DESeq2 
+#' ------------------------------------------------------------------------------
+create_te_analysis_panel <- function(deseq_data, 
+                                     tissue_name, 
+                                     order_colors,
+                                     fdr_threshold = 0.05,
+                                     base_font_size = 10,
+                                     min_font_size = 8,
+                                     font_family = 'sans',
+                                     section = 'full',
+                                     ...) {
+    
+    # Volcano plot: all TEs
+    all_volcano <- volcanoPlot(cutPvalue(deseq_data), FDR = fdr_threshold) +
+        labs(title = paste('DESeq of TEs in', tissue_name)) +
+        theme_rob(base_size = base_font_size, base_family = font_family) +
+        theme(legend.position = 'none')
+    
+    # Downregulated TEs
+    down_detes <- deseq_data %>% 
+        filter(log2FoldChange < 0, padj <= fdr_threshold)
+    
+    kimura_down_pl <- ggplot(down_detes, aes(Kimura, fill = order)) +
+        geom_histogram(position = 'stack', color = 'black', binwidth = 1) +
+        scale_fill_manual(values = order_colors) +
+        scale_x_continuous(breaks = seq(0, 50, 5)) +
+        scale_y_continuous(expand = c(0, 0)) +
+        labs(title = 'Downregulated TEs') +
+        theme_bw(base_size = min_font_size) +
+        theme(plot.title = element_text(hjust = 0.5, size = base_font_size),
+              legend.position = 'none')
+    
+    # Upregulated TEs
+    up_detes <- deseq_data %>% 
+        filter(log2FoldChange > 0, padj <= fdr_threshold)
+    
+    kimura_up_pl <- ggplot(up_detes, aes(Kimura, fill = order)) +
+        geom_histogram(position = 'stack', color = 'black', binwidth = 1) +
+        scale_fill_manual(values = order_colors) +
+        scale_x_continuous(breaks = seq(0, 50, 5)) +
+        scale_y_continuous(expand = c(0, 0)) +
+        labs(title = 'Upregulated TEs') +
+        theme_bw(base_size = min_font_size) +
+        theme(plot.title = element_text(hjust = 0.5, size = base_font_size),
+              legend.position = 'none')
+    
+    # Young TEs (Kimura <= 5)
+    deseq_young <- deseq_data %>% filter(Kimura <= 5)
+    
+    young_volcano <- volcanoPlot(cutPvalue(deseq_young), FDR = fdr_threshold) +
+        labs(title = 'Young TEs (Kimura \u2264 5)') +
+        theme_rob(base_size = base_font_size, base_family = font_family) +
+        theme(legend.position = 'none')
+    
+    # Middle-age TEs (5 < Kimura <= 25)
+    deseq_middle <- deseq_data %>% 
+        filter(dplyr::between(Kimura, 5.001, 25))
+    
+    middle_volcano <- volcanoPlot(cutPvalue(deseq_middle), FDR = fdr_threshold) +
+        labs(title = 'Middle old TEs (Kimura 5.001-25)') +
+        theme_rob(base_size = base_font_size, base_family = font_family) +
+        theme(legend.position = 'none')
+    
+    # Old TEs (Kimura > 25)
+    deseq_old <- deseq_data %>% filter(Kimura > 25)
+    
+    old_volcano <- volcanoPlot(cutPvalue(deseq_old), FDR = fdr_threshold) +
+        labs(title = 'Old TEs (Kimura > 25)') +
+        theme_rob(base_size = base_font_size, base_family = font_family) +
+        theme(legend.position = 'none')
+    
+    # Create combined plot with legend below
+    if (section == 'top'){
+        combined_plot <- ggarrange(
+            kimura_down_pl, all_volcano, kimura_up_pl,
+            nrow = 1, ncol = 3,
+            align = 'h',
+            ...)
+        
+        
+    }else{
+        combined_plot <- ggarrange(
+            kimura_down_pl, all_volcano, kimura_up_pl, 
+            young_volcano, middle_volcano, old_volcano, 
+            nrow = 2, ncol = 3,
+            align = 'h',
+            ...
+    )
+        
+    }
+    
+    return(combined_plot)
+}
