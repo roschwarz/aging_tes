823805e2711c531d45556d16b612ec1453835770
rschwarz <robert.schwarz@leibniz-fli.de>
2025-12-09 14:39:11 +0100

    scan for te island nanopore coverage

--- src/te_island/verification_by_nanoPore.R
+++ src/te_island/verification_by_nanoPore.R
@@ -28,21 +28,29 @@ top_100 <- counts %>%
     head(100)
 
 
-top_100 <- merge(top_100, brain_intergenic_te_islands, by.x = 'Name', by.y = 'V4')
+###### Islands of interest ####
+tei_of_interest <- counts %>% 
+    filter(Name %in% c("TE_Cluster_728986", "TE_Cluster_728983"))
+
+###### TEs for genome browser shot ########
+tes_for_gbs <- rbind(top_100, tei_of_interest) %>% unique()
+
+
+
+tes_for_gbs <- merge(tes_for_gbs, brain_intergenic_te_islands, by.x = 'Name', by.y = 'V4')
 
 # add coordinated as column like chr1:1000-2000
 
-top_100 <- top_100 %>%
+tes_for_gbs <- tes_for_gbs %>%
     mutate(coordinates = paste0(V1, ":", V2, "-", V3)) #%>%
     #select(Name, Length, TPM, coordinates, everything())
-
 
 # Iterate over top 20 candidates and create genome browser shots and store them in 
 # results/te_island/long_read_verification/genome_browser_shots
 
-for (island in top_100$Name) {
+for (island in tes_for_gbs$Name) {
     print(island)
-    te_island_info <- top_100 %>% filter(Name == island)
+    te_island_info <- tes_for_gbs %>% filter(Name == island)
     
     te_island = GRanges(
         seqnames = te_island_info$V1,
@@ -62,8 +70,9 @@ for (island in top_100$Name) {
     )
     
     if (!is.null(tracks)) {
+        message("Creating and store plot for ", island)
         png(outfile, width = 10, height = 6, units = "in", res = 300)
-           plotGenomeBrowserNanopore(tracks, te_island)
+           grid.draw(plotGenomeBrowserNanopore(tracks, te_island))
         dev.off()
     } else {
         # Optionally message or log that no plot was created for this island
@@ -72,3 +81,48 @@ for (island in top_100$Name) {
    
 }
 
+
+# -----------------------------------------------------------------------------------------------------------
+# Genome Browser Shots
+# -----------------------------------------------------------------------------------------------------------
+
+####################
+# TE island 305170 
+# chr12:21560236-21562700 #
+####################
+
+start_coord = 21560236
+end_coord = 21562700
+
+island <- tes_for_gbs %>% filter(Name == "TE_Cluster_305170")
+
+te_island = GRanges(
+    seqnames = island$V1,
+    ranges = IRanges(start = island$V2 - 1000, end = island$V3 + 1000),
+    strand = island$V6
+)
+
+tracks <- genomeBrowserNanoporeTracks(
+    te_island_id = c("TE_Cluster_305170"),
+    te = te_island,
+    te_island_annotation_file = te_island_file_5_prime_extended,
+    bam_file = bam_file_dataset_1
+)
+
+meta <- list(name = 'genome_browser_shot_TE_island_305170',
+             description = 'Genome browser shot showing the TE island TE_Cluster_305170 with supporting nanopore reads.',
+             tags = c('TE island', 'nanopore', 'genome browser'),
+             parameters = list(te_island_id = "TE_Cluster_305170",
+                               coordinates = "chr12:21560236-21562700"),
+             script = 'verification_by_nanopore.R'
+)
+
+pl <- plotGenomeBrowserNanopore(tracks, te_island)
+                    
+fig_index(plot = pl,
+          outdir = figure_dir,
+          meta = meta,
+          index_file = figure_index_file,
+          width = 15,
+          height = 10,
+          dpi = 300)
