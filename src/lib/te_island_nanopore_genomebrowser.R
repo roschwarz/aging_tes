98f443be65cc00fb9c51b32ccab7c05989f99d89
rschwarz <robert.schwarz@leibniz-fli.de>
2025-12-09 14:22:15 +0100

    Nanopore tracks
    functions to provide genome browser plots for nanopore data sets

--- src/lib/te_island_nanopore_genomebrowser.R
+++ src/lib/te_island_nanopore_genomebrowser.R
@@ -19,14 +19,24 @@ by_start_read <- function(aln, max_reads, strand){
     
 }
 
+by_read_length <- function(aln, max_reads, strand){
+    
+    aln <- aln[order(width(aln), decreasing = TRUE)][1:max_reads]
+    
+    return(aln)
+    
+}
+
 by_start_te_island <- function(aln, max_reads, strand, te){
     
+    message("Filtering reads by start position relative to TE island...")
+    message(paste0("Total n reads before filtering: ", length(aln)))
     read_starts <- granges(aln)
     
     if (as.character(strand(te)) == "+"){
         
         start_coord = start(te) - 1000
-        end_coord = start(te) + 1000
+        end_coord = end(te) + 1000
         window <- GenomicRanges::GRanges(seqnames = as.character(seqnames(te)),
                                          ranges = IRanges::IRanges(start = start_coord,
                                                                   end = end_coord),
@@ -37,7 +47,7 @@ by_start_te_island <- function(aln, max_reads, strand, te){
         
     } else {
         
-        start_coord = end(te) - 1000
+        start_coord = start(te) - 1000
         end_coord = end(te) + 1000
         
         window <- GenomicRanges::GRanges(seqnames = as.character(seqnames(te)),
@@ -50,7 +60,16 @@ by_start_te_island <- function(aln, max_reads, strand, te){
     }
     
    
+   message(paste0("Filtering reads starting within: ", as.character(window)))
+   message(paste0("Window start: ", start_coord, " end: ", end_coord))
+   message(paste0("Total n reads before filtering: ", length(read_starts)))
+    
+   # browser()
+    
    hits <- findOverlaps(read_starts, window)
+   
+   message(paste0("n reads overlapping the window: ", length(hits)))
+   
    if (length(hits) < max_reads){
        max_reads = length(hits)
    }
@@ -61,12 +80,15 @@ by_start_te_island <- function(aln, max_reads, strand, te){
        filtered_reads <- NULL
    }
    
+   message(paste0("n reads after filtering: ", length(filtered_reads)))
+   
    return(filtered_reads)
     
 }
 
-sub_sampling_reads <- function(bam_file, chromosome, start_coord, end_coord, strand, max_reads = 25, sampling_strat = 'by_pos'){
+sub_sampling_reads <- function(bam_file, chromosome, start_coord, end_coord, strand, max_reads = 30, sampling_strat = 'by_pos'){
     
+    message(paste0("Reading alignments from BAM file: ", bam_file))
     which <- GenomicRanges::GRanges(seqnames = chromosome,
                                     ranges = IRanges::IRanges(start = start_coord, end = end_coord),
                                     strand = strand)
@@ -74,15 +96,22 @@ sub_sampling_reads <- function(bam_file, chromosome, start_coord, end_coord, str
     param <- Rsamtools::ScanBamParam(which = which)
     
     aln <- GenomicAlignments::readGAlignments(bam_file, param = param)
+    
+    message(paste0("n alignments: ", length(aln)))
     # Sub sample to avoid plotting too many reads
     if (length(aln) > max_reads) {
         if (sampling_strat == 'by_pos') {
             print("Sampling by position...")
             aln <- by_start_read(aln, max_reads, strand = strand)
+        }
+        else if (sampling_strat == 'by_length') {
+            print("Sampling by read length...")
+            aln <- by_read_length(aln, max_reads, strand = strand)
+        }
         #} else if(sampling_strat == 'by_te'){
         #    print("Sampling by TE...")
         #    aln <- by_start_te_island(aln, max_reads, strand, which)
-        }
+        
         else {
             print("Random sampling...")
             aln <- random_sampling(aln, max_reads)
@@ -99,7 +128,6 @@ sub_sampling_reads <- function(bam_file, chromosome, start_coord, end_coord, str
 
 
 te_island_track <- function(te_island_id_list,
-                            te_island_ranges,
                             te_island_annotation_file){
     
     te_island_ranges <- genomation::readGeneric(te_island_annotation_file, 
@@ -114,9 +142,17 @@ te_island_track <- function(te_island_id_list,
                     featureAnnotation = "id",
                     fontcolor.item = "white",
                     fill = "black",
-                    size = 2)
+                    size = 0.5)
 }
 
+coloring_te_instances <- function(te_instance_class_list){
+    # map the order.color to the te_instance_class_list to get a list of colors that should be returned by this
+    # function
+    colors <- order.color[te_instance_class_list]
+    colors[is.na(colors)] <- "black"
+    return(colors)
+    
+}
 
 local_te_track <- function(te_ranges, te){
     
@@ -127,6 +163,8 @@ local_te_track <- function(te_ranges, te){
                dplyr::between(start, start(te), end(te))) %>% 
         blackRcloud::splitTEID("te.id") %>% 
         unique()
+    
+    color <- coloring_te_instances(local_tes$order)
         
     ranges_of_interest <- te_ranges[te_ranges@elementMetadata$te.id %in% local_tes$te.id & !duplicated(te_ranges@elementMetadata$te.id)]
     ids_of_ranges_of_interest <- ranges_of_interest %>% 
@@ -136,37 +174,42 @@ local_te_track <- function(te_ranges, te){
     
     te_instance_track <- AnnotationTrack(ranges_of_interest,
                                   featureAnnotation = "id",
+                                  name = 'TE instances',
                                   id = ids_of_ranges_of_interest,
                                   stacking = "dense",
-                                  col = 'white',
-                                  size = 2)
+                                  #col = 'white',
+                                  fill = color,
+                                  size = 0.5)
     
     return(te_instance_track)
 }
 
 
-nanopore_track <- function(te, bam_file, max_reads = 25, name = "nanopore reads"){
+nanopore_track <- function(te, bam_file, max_reads = 25, name = "nanopore reads", sampling_strat = 'by_te'){
 
     
     readRanges <- sub_sampling_reads(bam_file, 
                                      as.character(seqnames(te)), 
-                                     start(te), 
-                                     end(te),
+                                     start(te) - 1500, 
+                                     end(te) + 5000,
                                      as.character(strand(te)),
                                      max_reads = max_reads,
-                                     sampling_strat = 'by_te')
+                                     sampling_strat = sampling_strat)
     
-    if(is.null(readRanges)){
+    if (is.null(readRanges)) {
         return(NULL)
     }
+    
+    message(paste0("n reads to plot: ", length(readRanges)))
     
     nanopore_reads <- Gviz::AnnotationTrack(
         range = readRanges,
         genome = "mm10",
-        name = "reads",
+        name = "nanopore reads",
         fill = "lightgray",
         chromosome = as.character(seqnames(te)),
-        id = c()
+        id = c(),
+        size = 0.5
     )
     
 }
@@ -212,7 +255,7 @@ genomeBrowserNanoporeTracks <- function(te_island_id,
                                   bam_file){
     
     gtrack <- GenomeAxisTrack()
-    te_island_track <- te_island_track(te_island_id_list = te_island_id, te_island_ranges, te_island_annotation_file)
+    te_island_track <- te_island_track(te_island_id_list = te_island_id, te_island_annotation_file)
     te_instance_track <- local_te_track(teRanges, te)
     nanopore_track <- nanopore_track(te, bam_file) 
     cage_tracks <- cage_seq_tracks(te)
@@ -221,7 +264,7 @@ genomeBrowserNanoporeTracks <- function(te_island_id,
         return(NULL)
     }
     
-    list(
+    tracks <- list(
         #ideoTrack,
         gtrack,
         te_island_track,
@@ -230,21 +273,24 @@ genomeBrowserNanoporeTracks <- function(te_island_id,
         nanopore_track
     )
     
+    return(tracks)
+    
+    
 }
 
 plotGenomeBrowserNanopore <- function(track_list, te){
-    Gviz::plotTracks(
+    grid.grabExpr(Gviz::plotTracks(
         unname(track_list),
         #add35 = TRUE,
         shape = 'box',
         scale = 0.25,
         #chromosome = seqnames(te),
-        from = start(te) - 1000,
-        to = end(te) + 1000,
+        from = start(te) - 1500,
+        to = end(te) + 1500,
         col.axis = "black",
         col.title = "black",
         cex.feature = 0.7,
         background.title = "white",
         fontcolor.legend = "black"
-    )
+    ))
 }
