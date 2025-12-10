396ad143f2eddffe23619e0799daf8768fc805ef
rschwarz <robert.schwarz@leibniz-fli.de>
2025-12-10 11:18:44 +0100

    add function to environment to load quant Range object

--- env/R/quant_env.R
+++ env/R/quant_env.R
@@ -30,3 +30,38 @@ load_quant_seq_env <- function(){
     
     
 }
+
+
+#' Loading the annotation of peaks as a gRange object
+#'
+#' @export
+load_quant_peak_annotation <- function(){
+    
+    logmsg("→ Loading annotation of CAGE peaks as gRange object...")
+    
+    quant_beds <- list('brain' ='results/quant_seq/brain/raw_peaks/brain.quant_peaks.bed',
+                       'skin' = 'results/quant_seq/skinII/raw_peaks/skin.quant_peaks.bed',
+                       'blood' = 'results/quant_seq/blood/raw_peaks/blood.quant_peaks.bed')
+    
+    
+    quantRanges <<- sapply(names(quant_beds), simplify = F, function(tissue){
+        
+        
+        file <- quant_beds[[tissue]]
+        if (file.exists(file)){
+            return(readGeneric(file, strand = 6, meta.cols = list(names = 4)))
+        }else{
+            logmsg(paste('Raw quant peak annotation does not exist for', tissue, '. Please check quant_store_annotation.R'))
+        }
+        
+    })
+    
+    base_path <- paste0(getwd(), "/env/R/load_data")
+    
+    annotation_files <- c("annotations.R")
+    
+    for (f in annotation_files){
+        source(file.path(base_path, f))
+    }
+    
+}
