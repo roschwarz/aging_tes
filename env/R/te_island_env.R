e9866542c70b5a66a73452547a647daa29eede85
rschwarz <robert.schwarz@leibniz-fli.de>
2025-12-10 16:10:45 +0100

    add te_island_transcripts file to environment of te islands

--- env/R/te_island_env.R
+++ env/R/te_island_env.R
@@ -31,6 +31,16 @@ load_te_island_env <- function(){
                                  skin = "./results/te_island/skin_indie_te_island.bed",
                                  blood = "./results/te_island/blood_indie_te_island.bed")
     
+    
+    # ==============================================================================
+    # TE island transcripts s
+    # TE islands that overlap with at least one cage- and quant-peak and where at 
+    # least one transposable element is considered as expressed (p adjusted is not NA)
+    # The script to annotate them can be found in src/te_island_transcripts.R
+    # ==============================================================================
+    
+    te_island_transcripts <<- "results/TEItx/canonical_te_island_transcripts.csv"
+    
     # ==============================================================================
     # Verification of TE islands by nanopore
     # intergenic TEs in brain
@@ -43,4 +53,7 @@ load_te_island_env <- function(){
     cage_fwd_bw <<- 'results/te_island/brain_forward.bw'
     cage_rev_bw <<- 'results/te_island/brain_reverse.bw'
     cage_old_rev_bw <<- '/misc/paras/data/www/robert/mCQuaRna/cage/mm10/brain.old.reverse.bw'
+    
+    
+    
 }
